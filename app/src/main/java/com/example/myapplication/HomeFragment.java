ab4565b355870cd256a03ed145a2e765b9a7778d
CorinneMarie <corinne.portelli.14@um.edu.mt>
2019-05-11 12:42:24 +0200

    Alligned profile

--- app/src/main/java/com/example/myapplication/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/HomeFragment.java
@@ -7,12 +7,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+//import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
+    private static final String TAG = "ViewDatabase";
 
+    private FirebaseDatabase mFirebaseDatabase;
+    //private FirebaseAuth mAuth;
+    //private FirebaseAuth.AuthStateListener nAuthListener;
+    private DatabaseReference myRef;
+
+    private String name;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -22,8 +33,13 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+
     }
 
 }
