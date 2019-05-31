c6cc3fdb776d1365b3be47a8e3083d6acb9b5acb
ItzJNTZ <31597249+ItzJNTZ@users.noreply.github.com>
2019-05-31 05:10:24 +0200

    video update

--- app/src/main/java/com/example/myapplication/VideoActivity.java
+++ app/src/main/java/com/example/myapplication/VideoActivity.java
@@ -2,24 +2,42 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.youtube.player.*;
 
 public class VideoActivity extends  YouTubeBaseActivity {
 
-
+private TextView title;
+private Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
-        //final EditText editTextId = findViewById(R.id.editTextId);
-        //Button buttonPlay = findViewById(R.id.buttonPlay);
+        title = findViewById(R.id.txt_lesson_title);
+        title.setText(HomeFragment.curr_activity.getName());
+        back = findViewById(R.id.back);
+
+        back.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View arg0) {
+
+                finish();
+            }
+
+        });
+
+
         final YouTubePlayerView youtubePlayerView = findViewById(R.id.youtubePlayerView);
         String videoId = HomeFragment.curr_activity.getVideo();
         playVideo(videoId, youtubePlayerView);
+
+
 
 
     }
